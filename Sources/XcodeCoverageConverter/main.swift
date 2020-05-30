3413e5e7f1b2f80eb4edd92389ad64e9916ba0c1
Thibault Wittemberg <twittemberg@genetec.com>
2020-05-30 14:51:59 -0400

    main: add the --version option

--- Sources/XcodeCoverageConverter/main.swift
+++ Sources/XcodeCoverageConverter/main.swift
@@ -3,8 +3,9 @@ import ArgumentParser
 struct Xcc: ParsableCommand {
     static let configuration = CommandConfiguration(
         abstract: "A Swift command-line tool to convert xccov outputs into continuous integration friendly formats",
-        subcommands: [Generate.self]
-    )
+        version: "0.1",
+        subcommands: [Generate.self],
+        defaultSubcommand: Generate.self)
 
     init() { }
 }
